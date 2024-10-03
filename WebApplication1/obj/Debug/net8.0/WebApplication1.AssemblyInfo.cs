d60a388afd89240ff26cac8a3dc5a498b4776e79
DevlinW98 <80415171+DevlinW98@users.noreply.github.com>
2024-10-03 23:34:35 +0700

    Rollback

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeeca94f8c094ee173053120e25dd3dd7327cdd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdfcd50e1d250719d3175d7ec34fc1aa2558269b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
