1f9c4de005e91c81ff83f74ea657dedba9cd5e98
DevlinW98 <80415171+DevlinW98@users.noreply.github.com>
2024-10-03 23:45:10 +0700

    Delete_It_all

--- WebApplication1/wwwroot/js/login.js
+++
@@ -1,8 +0,0 @@
-﻿const currentUrl = window.location.href;
-const tabs = document.querySelectorAll(".tab");
-
-tabs.forEach(tab => {
-    if (currentUrl === tab.href) {
-        tab.style.borderBottom = "2px solid black";
-    }
-});
